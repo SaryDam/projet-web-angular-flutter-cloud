0665cf680d1f8e5acf43d8e6017ebd7b61abbb80
dcreux <d.creux@groupe-burus.tech>
2024-11-25 15:38:18 +0100

    rollback angular to 16

--- ma-to-do-web/src/app/app.component.ts
+++ ma-to-do-web/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: true,
-  styleUrl: './app.component.scss',
-  imports: [RouterModule], // Importation nécessaire pour `<router-outlet>`
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ma-to-do-web';
