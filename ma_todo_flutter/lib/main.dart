278cb30a4fdcae2e2c3a32f0b3013448b3de50a3
Serhat1304 <serhatkus01@gmail.com>
2024-11-26 11:35:13 +0100

    mise en place de firebase dans flutter

--- ma_todo_flutter/lib/main.dart
+++ ma_todo_flutter/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'screens/todo_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -9,9 +15,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Todo App',
-      home: TodoScreen(),
+      home: TaskScreen(),
+    );
+  }
+}
+
+class TaskScreen extends StatefulWidget {
+  @override
+  _TaskScreenState createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  final TextEditingController _taskController = TextEditingController();
+
+  Future<void> _addTask(String taskName) async {
+    if (taskName.isNotEmpty) {
+      try {
+        await FirebaseFirestore.instance.collection('tasks_flutter').add({
+          'name': taskName,
+          'created_at': FieldValue.serverTimestamp(),
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Task "$taskName" added!')),
+        );
+        _taskController.clear();
+      } catch (e) {
+        print('Error: $e');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to add task.')),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Task name cannot be empty.')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Task to Firestore'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _taskController,
+              decoration: InputDecoration(
+                labelText: 'Task Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => _addTask(_taskController.text),
+              child: Text('Add Task'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
